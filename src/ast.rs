22e49a562e171bf082175e9ae08a14811ce89022
Peter Malmgren <ptmalmgren@gmail.com>
2021-04-30 17:37:44 -0400

    Remove visitor pattern and use recursive traverse function

--- src/ast.rs
+++ src/ast.rs
@@ -11,57 +11,44 @@ pub enum Expression {
     Unary(parse::Token, Box<Expression>),
 }
 
-impl Expression {
-    fn accept<T>(self: &Box<Self>, visitor: &dyn Visitor<T>) -> T {
-        visitor.visit(self)
-    }
-}
-
-trait Visitor<T> {
-    fn visit(&self, expr: &Box<Expression>) -> T;
-}
-
 pub struct AstPrinter {
-    pub ast: Box<Expression>,
+    ast: Box<Expression>,
 }
 
-impl Visitor<String> for AstPrinter {
-    fn visit(&self, expr: &Box<Expression>) -> String {
+impl AstPrinter {
+    fn traverse(&self, expr: &Box<Expression>) -> String {
         match expr.as_ref() {
             Expression::Binary(lhs, tok, rhs) => {
-                format!("({} {} {})", tok.lexeme.as_str(), &*lhs.accept(self).as_str(), &*rhs.accept(self).as_str())
-            },
+                format!(
+                    "({} {} {})",
+                    tok.lexeme.as_str(),
+                    self.traverse(&lhs),
+                    self.traverse(&rhs)
+                )
+            }
             Expression::Grouping(expr) => {
-                format!("(group {})", &*expr.accept(self))
-            },
+                format!("(group {})", self.traverse(&expr))
+            }
             Expression::Literal(lit) => {
                 format!("{}", lit)
-            },
+            }
             Expression::Unary(tok, expr) => {
-                format!("({} {})", tok.lexeme, &*expr.accept(self))
-            },
+                format!("({} {})", tok.lexeme, self.traverse(&expr))
+            }
         }
     }
 }
 
 impl ToString for AstPrinter {
     fn to_string(&self) -> String {
-        let ast = &self.ast;
-        let s = ast.accept(self);
-        s.to_string()
-    }
-}
-
-impl AstPrinter {
-    fn print_ast(&self) {
-        println!("{}", self.to_string());
+        self.traverse(&self.ast)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use parse::{Token, TokenType, Literal};
+    use parse::{Literal, Token, TokenType};
 
     fn addition(num1: i64, num2: i64) -> Box<Expression> {
         let num1 = Literal::Number(num1);
@@ -70,7 +57,7 @@ mod tests {
             TokenType::Plus,
             "+".to_string(),
             Literal::Symbol("+".to_string()),
-            1
+            1,
         );
         Box::new(Expression::Binary(
             Box::new(Expression::Literal(num1)),
@@ -84,7 +71,7 @@ mod tests {
             TokenType::Minus,
             "-".to_string(),
             Literal::Symbol("-".to_string()),
-            1
+            1,
         );
         Box::new(Expression::Unary(negate, expr))
     }
